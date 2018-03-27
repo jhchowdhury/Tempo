981e7eff21b8b6f3063867446c975e61d002ead5
KaanKiranbay <kaanvoignar@windowslive.com>
2018-03-27 21:19:43 +0300

    + added attributes to RegisterManager + added register() and operation() methods to RegisterManager but I DIDNOT fill them

--- src/tempo/Authorization/RegisterManager.java
+++ src/tempo/Authorization/RegisterManager.java
@@ -1,4 +1,17 @@
 package tempo.Authorization;
 
 public class RegisterManager {
+
+    private String name;
+    private String surname;
+    private String username;
+    private String password;
+
+    public void register(){
+
+    }
+
+    public void operation(){
+
+    }
 }
