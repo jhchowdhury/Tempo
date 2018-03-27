c5cc5cda0655e333f2a674bda5df2752bae83732
KaanKiranbay <kaanvoignar@windowslive.com>
2018-03-27 22:09:19 +0300

    + added methods to EventBus but didnt fill them added attributes to Notification class and added the getAction method but didnt implement it

--- src/tempo/EventManagement/EventController.java
+++ src/tempo/EventManagement/EventController.java
@@ -15,6 +15,6 @@ public class EventController {
     }
 
     public void operation(){
-        
+
     }
 }
--- src/tempo/NotificationManagement/EventBus.java
+++ src/tempo/NotificationManagement/EventBus.java
@@ -1,4 +1,12 @@
 package tempo.NotificationManagement;
 
 public class EventBus {
+
+    public void refreshFriendList(){
+
+    }
+
+    public void refreshNotification(){
+
+    }
 }
