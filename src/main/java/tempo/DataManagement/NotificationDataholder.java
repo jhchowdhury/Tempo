c71d349fcde8de5616c1690712fdadd2c0ca4d17
KaanKiranbay <kaanvoignar@windowslive.com>
2018-04-28 13:47:44 +0300

    #deleted dataholders from data management

---
+++ src/main/java/tempo/DataManagement/NotificationDataholder.java
@@ -0,0 +1,9 @@
+package tempo.DataManagement;
+
+import tempo.NotificationManagement.Notification;
+
+import java.util.ArrayList;
+
+public class NotificationDataholder {
+    private ArrayList<Notification> notificationList = new ArrayList<>();
+}
