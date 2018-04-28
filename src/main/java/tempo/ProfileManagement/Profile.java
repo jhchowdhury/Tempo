411550a518d36a9463c9888edc680ee58748bef4
KaanKiranbay <kaanvoignar@windowslive.com>
2018-04-28 14:53:00 +0300

    #deleted dataholders from data management

--- src/main/java/tempo/ProfileManagement/Profile.java
+++ src/main/java/tempo/ProfileManagement/Profile.java
@@ -3,17 +3,11 @@ package tempo.ProfileManagement;
 import org.jongo.marshall.jackson.oid.MongoId;
 import org.jongo.marshall.jackson.oid.MongoObjectId;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;//BufferedImage
-import java.io.File;//to read File
-import java.io.IOException;
-
 public class Profile {
     private int profileID;
     private String name;
     private String email;
     private String profilePictureName;
-    private BufferedImage profileImage;
     private String[] preferences;
 
     @MongoId
--- src/main/java/tempo/SmartEventManagement/SmartEvent.java
+++ src/main/java/tempo/SmartEventManagement/SmartEvent.java
@@ -1,11 +1,11 @@
 package tempo.SmartEventManagement;
 
+import tempo.EventManagement.Event;
 import tempo.ProfileManagement.Friend;
 
 import java.util.ArrayList;
 
-public class SmartEvent {
-
+public class SmartEvent extends Event{
     private ArrayList<Friend> friendForEvent = new ArrayList<>();
     private int approval;
 
