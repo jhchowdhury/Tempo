e0bf95973fdcf97b0ce8a0fb718bc48e644566d7
KaanKiranbay <kaanvoignar@windowslive.com>
2018-05-04 18:03:42 +0300

    #changed Main

--- src/main/java/tempo/UserInterface/Main.java
+++ src/main/java/tempo/UserInterface/Main.java
@@ -1,6 +1,7 @@
 package tempo.UserInterface;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,18 +9,21 @@ import javafx.stage.Stage;
 import tempo.DataManagement.CommunicationHelper;
 
 public class Main extends Application {
-
+    Parent root;
+    Stage stage;
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("LoginPage.fxml"));
-        StageHolder.getInstance().stage = primaryStage;
+        this.root = FXMLLoader.load(getClass().getClassLoader().getResource("LoginPage.fxml"));
+        Scene login = new Scene(root);
         primaryStage.setTitle("Tempo App");
-        primaryStage.setScene(new Scene(root, 640, 360));
+        primaryStage.setScene(login);
         primaryStage.show();
+        this.stage = primaryStage;
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
