cfb355b5dbe8f4f2dc7c4c8d7f446188484ecb21
KaanKiranbay <kaanvoignar@windowslive.com>
2018-05-04 23:41:17 +0300

    #added MainView, added transition button actions

--- src/main/java/tempo/UserInterface/AuthorizationView.java
+++ src/main/java/tempo/UserInterface/AuthorizationView.java
@@ -53,12 +53,6 @@ public class AuthorizationView {
     private Label lblRegisterStatus;
 
     @FXML
-    private CalendarView calenderView;
-
-    @FXML
-    private Label dataBar;
-
-    @FXML
     private void Login(ActionEvent event) throws Exception{
         LoginManager login = new LoginManager();
         login.setUsername(txtLoginUsername.getText());
@@ -78,60 +72,6 @@ public class AuthorizationView {
 
     @FXML
     private void Register(ActionEvent event) throws Exception{
-
-    }
-    
-
-    @FXML
-    private void handleEventAddBtn(ActionEvent event) {
-        // Button was clicked, do something...
-        dataBar.setText("Button Action\n");
-        Popup pop = new Popup();
-        pop.display();
-    }
-
-    @FXML
-    private void handleProfileBtn(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("ProfilePage.fxml"));
-        StageHolder.getInstance().stage.setTitle("Tempo App");
-        StageHolder.getInstance().stage.hide();
-        StageHolder.getInstance().stage.setScene(new Scene(root, 1280, 720));
-        StageHolder.getInstance().stage.show();
-    }
-
-    private class Popup {
-
-        public String title;
-        public Date start, end;
-
-        private void display()
-        {
-            Stage popupwindow=new Stage();
-            popupwindow.initModality(Modality.APPLICATION_MODAL);
-            popupwindow.setTitle("Add Event Window");
-            Label label1= new Label("Enter a title:");
-            TextField txt1 = new TextField();
-            Label label2= new Label("Enter the start date:");
-            DatePicker txt2 = new DatePicker();
-            Label label3= new Label("Enter the end date:");
-            DatePicker txt3 = new DatePicker();
-            Button button1= new Button("Create an Event");
-
-            button1.setOnAction(e -> {
-                title = txt1.getText();
-                start = new Date(txt2.getValue().getYear(), txt2.getValue().getMonthValue(), txt2.getValue().getDayOfMonth());
-                end = new Date(txt3.getValue().getYear(), txt3.getValue().getMonthValue(), txt3.getValue().getDayOfMonth());
-                calenderView.addEvent(1,title, start, end);
-                popupwindow.close();
-            });
-            VBox layout= new VBox(10);
-            layout.getChildren().addAll(label1, txt1, label2, txt2, label3, txt3, button1);
-            layout.setAlignment(Pos.CENTER);
-            Scene scene1= new Scene(layout, 300, 250);
-            popupwindow.setScene(scene1);
-            popupwindow.showAndWait();
-
-        }
 
     }
 }
--- src/main/java/tempo/UserInterface/ProfileView.java
+++ src/main/java/tempo/UserInterface/ProfileView.java
@@ -1,4 +1,29 @@
 package tempo.UserInterface;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class ProfileView {
+    @FXML
+    private void logOutFromDatabase(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("LoginPage.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void goBackToCalendar(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainView.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
